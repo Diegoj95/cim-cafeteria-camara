31d04e6b6c486dae77b3b6ad0d9af71c0c688200
Ignacio Pardo <ignacio.pardo1901@alumnos.ubiobio.cl>
2023-04-02 01:51:27 -0400

    Se modifico el analisis de video por imagenes

--- main.py
+++ main.py
@@ -3,29 +3,22 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import time
 
-# Define las credenciales y autoriza el cliente
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-credenciales = ServiceAccountCredentials.from_json_keyfile_name("gs_credentials.json", scope)
-cliente = gspread.authorize(credenciales)
+def credenciales():
+    # Define las credenciales y autoriza el cliente
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    credenciales = ServiceAccountCredentials.from_json_keyfile_name("cim-cafeteria-camara/gs_credentials.json", scope)
+    cliente = gspread.authorize(credenciales)
+    # Abre la hoja de cálculo
+    hoja_calculo = cliente.open("DatosCafeteria")
+    # Selecciona la hoja de cálculo en la que se escribirán los datos
+    hoja = hoja_calculo.worksheet("Sheet1")
+    return hoja
 
-# Abre la hoja de cálculo
-hoja_calculo = cliente.open("DatosCafeteria")
+def cargar_imagen(intervalo, umbral):
+    # Carga la imagen de la cámara
+    #camara = cv2.VideoCapture(0)
 
-# Selecciona la hoja de cálculo en la que se escribirán los datos
-hoja = hoja_calculo.worksheet("Sheet1")
-
-# Define el intervalo de tiempo en segundos para enviar los datos
-intervalo = 10
-
-# Define el umbral para la umbralización de la imagen
-umbral = 200
-
-# Carga la imagen de la cámara
-camara = cv2.VideoCapture(0)
-
-while True:
-    # Captura la imagen de la cámara
-    ret, imagen = camara.read()
+    imagen = cv2.imread("imagen.PNG")
 
     # Convierte la imagen a escala de grises y aplica un filtro Gaussiano
     gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
@@ -37,6 +30,10 @@ while True:
     # Realiza una operación de dilatación para unir las áreas de píxeles blancos que puedan estar separados
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     umbralizada = cv2.dilate(umbralizada, kernel, iterations=2)
+    detectar_contornos(imagen, umbralizada, intervalo, umbral)
+
+
+def detectar_contornos(imagen, umbralizada, intervalo, umbral):
 
     # Encuentra los contornos de los objetos blancos en la imagen
     contornos, _ = cv2.findContours(umbralizada, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -46,7 +43,7 @@ while True:
     for contorno in contornos:
         perimetro = cv2.arcLength(contorno, True)
         aprox = cv2.approxPolyDP(contorno, 0.02 * perimetro, True)
-        if len(aprox) == 4 and cv2.contourArea(aprox) > 1000:
+        if len(aprox) == 4 and cv2.contourArea(aprox) > 500:
             mesas.append(aprox)
 
     # Dibuja un rectángulo alrededor de cada mesa y calcula su área para determinar si está ocupada o no
@@ -56,7 +53,7 @@ while True:
         x, y, w, h = cv2.boundingRect(mesa)
         cv2.rectangle(imagen, (x, y), (x + w, y + h), (0, 0, 255), 2)
         area = w * h
-        if area > 10000:
+        if area > 1000:
             mesas_ocupadas += 1
         else:
             mesas_disponibles += 1
@@ -66,6 +63,7 @@ while True:
 
     # Escribe los datos en la hoja de cálculo
     datos = [mesas_disponibles, mesas_ocupadas, fecha_hora]
+    hoja = credenciales()
     hoja.append_row(datos)
 
     # Espera el tiempo definido en el intervalo antes de tomar la siguiente imagen
@@ -74,14 +72,16 @@ while True:
     # Muestra la imagen con los rectángulos de las mesas
     cv2.imshow("Mesas de la cafetería", imagen)
 
-    # Espera a que se presione la tecla 'q' para salir del bucle
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    
 
-camara.release()
-cv2.destroyAllWindows()
-
-
+if __name__ == "__main__":
+    # Define el intervalo de tiempo en segundos para enviar los datos
+    intervalo = 10
+    # Define el umbral para la umbralización de la imagen
+    umbral = 200
+    cargar_imagen(intervalo, umbral)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
 
